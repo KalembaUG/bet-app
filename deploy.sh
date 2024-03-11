85a9e746633acff5ef9293cfb6723d05e1f8a924
KalembaUG <kalembajamil01@gmail.com>
2024-03-11 16:35:43 +0300

    config workflows

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+name: Deploy
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Build
+        run: |
+          npm install
+          npm run build
+      
+      - name: Send Deployment Webhook
+        run: |
+          curl -X POST -H "Content-Type: application/json" -d '{"ref": "${{ github.ref }}", "repository": "${{ github.repository }}"}' https://webhooks.hostinger.com/deploy/42174b59ec72313c758f529c511feb0a
